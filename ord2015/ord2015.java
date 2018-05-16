14de7541b224f4d298a6c89e0a3f33816d068219
100346250 <100346250@alumnos.uc3m.es>
2018-05-16 10:43:18 +0200

    ord2015

--- ord2015/ord2015.java
+++ ord2015/ord2015.java
@@ -1,4 +1,14 @@
-import org.w3c.dom.*;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 class ord2015 {
 
