47d9f199b322e3e699f9f837fe114719bf2bf1b4
100346250 <100346250@alumnos.uc3m.es>
2018-05-16 10:28:35 +0200

    ord 2015

---
+++ ord2015/ord2015.java
@@ -0,0 +1,33 @@
+private static String ordinaria(Document doc, String referencia){
+  Element el1,el2, docE1;
+  NodeList nL, nL2;
+  int i, j, s1, s2;
+  Node n1, n2;
+  Página 1 de 3
+  docEl= doc.getDocumentElement();
+  nL= docEl.getChildNodes();
+  s1= nL.getLength();
+  if (s1>0) {
+   for (i=0; i<s1; i++) {
+    n1= nL.item(i);
+    if (n1.getNodeType() == Node.ELEMENT_NODE) {
+     el1= (Element) n1;
+     if (el1.getAttribute("id").equals(referencia)) {
+      nL2= el1.getChildNodes();
+      s2=nL2.getLength();
+      for(j=0; j<s2; j++) {
+       n2= nL2.item(j);
+       if (n2.getNodeType() == Node.ELEMENT_NODE) {
+        el2= (Element) n2;
+        if (el2.getNodeName().equals("nombre")) {
+         System.out.println("Encontrado");
+         return el2.getFirstChild().getNodeValue();
+        }
+       }
+      }
+     }
+    }
+     }
+  }
+  return "";
+}
