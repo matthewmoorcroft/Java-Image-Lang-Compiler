4f404342f21881573c9fa31e10ad80cd14f0acb4
100346250 <100346250@alumnos.uc3m.es>
2018-05-17 13:00:25 +0200

    exercise

--- Ejercicios/Ejercicio3/Films.java
+++ Ejercicios/Ejercicio3/Films.java
@@ -140,7 +140,7 @@ public class Films {
             infoList = film.getChildNodes();
             sizeI = infoList.getLength();
             direc = film.getAttributes();
-            System.out.println("Size of direc: " + direc);
+            System.out.println("Size of direc: " + direc.getLength());
               currentI = direc.getNamedItem("Nombre");
                 info = (Element) currentI;
                 film_attr = info.getAttributeNode("Nombre");
